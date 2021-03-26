a5ab81b07cff0d1af58257daf6c2027263131448
John Mwaura <johnmwaura08@gmail.com>
2021-03-26 06:37:55 -0500

    simple project completed with react redux and typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 import reportWebVitals from './reportWebVitals';
+import reducer from "./store/reducer"
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
